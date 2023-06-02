c4d30c7adf108237f8e74f227b6c5e9ec08f43c4
ArthurGabriel <arthurelgg@gmail.com>
2023-06-01 21:16:57 -0300

    :bulb: Transição suave no carregamento do app

--- guia_unb/lib/pages/onboarding/onboarding_page.dart
+++ guia_unb/lib/pages/onboarding/onboarding_page.dart
@@ -3,18 +3,35 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 import 'package:guia_unb/core/config/routes/routes.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   const OnboardingPage({Key? key}) : super(key: key);
+
+  @override
+  OnboardingPageState createState() => OnboardingPageState();
+}
+
+class OnboardingPageState extends State<OnboardingPage> {
+  bool _showContent = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _showContent = true;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return AnimatedOpacity(
-      opacity: 1,
-      duration: const Duration(milliseconds: 500),
-      child: Scaffold(
-        body: SafeArea(
+    return Scaffold(
+      body: AnimatedOpacity(
+        opacity: _showContent ? 1.0 : 0.0,
+        duration: const Duration(milliseconds: 700),
+        child: SafeArea(
           child: Stack(
             children: [
               Center(
@@ -34,32 +51,30 @@ class OnboardingPage extends StatelessWidget {
               Center(
                 child: Column(
                   children: [
-                    Text("Bem vindo", style: theme.textTheme.titleLarge),
+                    Text(
+                      "Bem vindo",
+                      style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
+                    ),
                     Text(
                       "Obrigado por baixar o Guia UnB",
                       style: theme.textTheme.labelMedium?.copyWith(
-                        color: Theme.of(context)
-                            .colorScheme
-                            .onSurface
-                            .withOpacity(0.9),
+                        color: Theme.of(context).colorScheme.onSurface.withOpacity(0.9),
                       ),
                     ),
                     const SizedBox(height: 500),
                     ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                        textStyle: theme.textTheme.labelMedium,
-                        backgroundColor: theme.colorScheme.secondary,
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 14,
-                          horizontal: 32,
+                        textStyle: theme.textTheme.bodyMedium?.copyWith(
+                          color: theme.colorScheme.onSecondary,
                         ),
+                        backgroundColor: theme.colorScheme.secondary,
+                        padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 32),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(20),
                         ),
                         elevation: 10,
                       ),
-                      onPressed: () =>
-                          Navigator.of(context).pushNamed(Routes.home),
+                      onPressed: () => Navigator.of(context).pushNamed(Routes.home),
                       child: const Text("Começar"),
                     ),
                   ],
